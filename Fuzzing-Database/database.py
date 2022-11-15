5353a041733e8a0385fb3b38e28f2d8b9791ec16
IscarioteSXIII <jul.degraeve@gmail.com>
2022-11-15 12:44:07 +0100

    Update database.py

--- Fuzzing-Database/database.py
+++ Fuzzing-Database/database.py
@@ -1,1 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import argparse
+import pylibinjection
+import threading
+from multiprocessing.pool import ThreadPool as Pool
+lock = threading.Lock()
 
+
+#Colorisation du texte permettant une meilleure compréhension des résultats affichés
+
+def colorize(color, text):
+    if color == 'red':
+        return ''.join(['\033[1;31m', text, '\033[1;m'])
+    elif color == 'green':
+        return ''.join(['\033[1;32m', text, '\033[1;m'])
+    elif color == 'blue':
+        return ''.join(['\033[1;34m', text, '\033[1;m'])
+    else:
+        return text
+
+
+#Arguments et options permettant de choisir la BDD à fuzzer, qu'elle soit locale ou distante
+
+def parse_cli_args():
+    parser = argparse.ArgumentParser(
+        description='Fuzzer libinjection pour MariaDB, MSSQL, \
+            MySQL, PostgreSQL and Oracle DB')
+    parser.add_argument(
+        '-t',
+        '--type',
+        dest='type',
+        default='mysql',
+        help=('Database type: mysql'),
