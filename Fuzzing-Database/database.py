e5c91ec48cb63e85ac16a0bc4dcadb2fbfc14db7
IscarioteSXIII <jul.degraeve@gmail.com>
2022-11-15 18:46:50 +0100

    Update database.py

--- Fuzzing-Database/database.py
+++ Fuzzing-Database/database.py
@@ -93,10 +93,109 @@ def db_connect(args):
                 database=args.db,
                 host=args.host)
         except mysql.connector.Error as err:
-            print(colorize("red", "[ERROR] {}".format(err)))
+            print(colorize("red", "[ERREUR] {}".format(err)))
             return None
 
 
     return connection
+
+def get_next(string, args):
+    if len(string) <= 0:
+        string.append(args.chars[0])
+    else:
+        string[0] = args.chars[
+            (args.chars.index(string[0]) + 1) % len(args.chars)]
+        if args.chars.index(string[0]) == 0:
+            return list(string[0]) + get_next(string[1:], args)
+    return string
+
+#Sauvegarde des informations dans un fichier log
+
+def log_msg(filename, msg):
+    lock.acquire()
+    with open(filename, "a") as f:
+        f.write("{}\n".format(msg))
+    lock.release()
+
+def process_one(opts):
+    cursor = opts[0]
+    payload = opts[1]
+    args = opts[2]
+    if os.path.isfile("{}_fp.txt".format(args.type)):
+        fingerprints = open("{}_fp.txt".format(args.type), "r").read()
+    else:
+        fingerprints = list()
+
+    if args.type in ["mysql", "mariadb"]:
+        for item in cursor.execute(args.query.format(payload), multi=True):
+            rows = item.fetchall()
+    else:
+        cursor.execute(args.query.format(payload))
+        rows = cursor.fetchall()
+    sqli = pylibinjection.detect_sqli(payload)
+    msg = "Fingerprint: {} Query: {} Result: {}".format(
+        sqli["fingerprint"], args.query.format(payload), rows)
+    if len(rows) > 0:
+        if sqli["sqli"]:
+            print (colorize("red", "[BLOQUE] {}".format(msg)))
+            if args.log_all:
+                log_msg(
+                    "{}_bad.txt".format(
+                        args.type), "[{}] {}".format(
+                        args.type.upper(), msg))
+        else:
+            if sqli["fingerprint"] in fingerprints:
+                print (colorize("blue", "[OK][DUP] {}".format(msg)))
+                log_msg(
+                    "{}_bad.txt".format(
+                        args.type), "[DUPE][{}] {}".format(
+                        args.type.upper(), msg))
+            else:
+                print (colorize("green", "[OK][Nouveau] {}".format(msg)))
+                log_msg(
+                    "{}_good.txt".format(
+                        args.type), "[{}] {}".format(
+                        args.type.upper(), msg))
+                log_msg("{}_fp.txt".format(args.type), sqli["fingerprint"])
+                fingerprints.append(sqli["fingerprint"])
+
+
+def main(args):
+    cnx = db_connect(args)
+    if not cnx:
+        sys.exit()
+    cursor = cnx.cursor()
+    sequence = list()
+    if args.log_all:
+        file_log = open("{}_all.txt".format(args.type), "w")
+    if args.check:
+        payload = args.payload.format(args.check)
+        process_one([cursor, payload, args])
+        sys.exit()
+    else:
+        while True:
+            sequence = get_next(sequence, args)
+            item = ''.join(reversed(sequence))
+            if len(item) == 5:
+                cnx.close()
+                sys.exit()
+            payload = args.payload.format(item)
+            try:
+                process_one([cursor, payload, args])
+            except BaseException as err:
+                if args.type == "pgsql":
+                    cnx.rollback()
+                if args.log_all:
+                    log_msg(
+                        "{}_all.txt".format(
+                            args.type), "[{}] Query: {}".format(
+                            args.type.upper(), args.query.format(payload)))
+                continue
+
+
+if __name__ == "__main__":
+    args = parse_cli_args()
+    main(args)
+
 
 
